f7f3730a08f8e61becee1043b438e5fb9aec6c2c
subin0321 <subin0321p@gmail.com>
2025-01-03 00:22:09 +0900

    userPage삭제, 투표 페이지 수정, 폰트 수정

--- src/pages/UserMainPage.js
+++ src/pages/UserMainPage.js
@@ -109,7 +109,7 @@ const UserMainPage = () => {
     >
       {isVoteFinished && (
         <div className="absolute inset-0 bg-black bg-opacity-90 flex items-center justify-center z-50">
-          <p className="text-white text-3xl font-bold font-dosgothic">투표 시간이 완료되었습니다! 곧 투표 결과가 공개됩니다!</p>
+          <p className="text-white text-3xl font-bold font-dosgothic">투표 시간이 완료되었습니다! 곧 투표 결과가 공개 됩니다. </p>
         </div>
       )}
 
